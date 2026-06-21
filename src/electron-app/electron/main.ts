894e15bfb0efbad34a3918cb2241b7d41cc6486e
Oruetrash <olivervelaz78@gmail.com>
2026-06-20 21:30:37 -0300

    fix: Electron blank screen - disable webSecurity for file:// module scripts

--- src/electron-app/electron/main.ts
+++ src/electron-app/electron/main.ts
@@ -75,6 +75,7 @@ function createWindow() {
       preload: join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
+      webSecurity: false,
     },
   });
   startupLog(`Window created, __dirname=${__dirname}`);
